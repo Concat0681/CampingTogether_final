fa8b2522efbe651461b96d5d60522ec7460a7e7a
SHyeBin <126125454+SHyeBin@users.noreply.github.com>
2023-03-31 17:03:11 +0900

    중고장터 insert작업중
    photo insert 중

--- Camping_Together/src/main/java/kr/or/iei/usedBoard/model/vo/UsedBoardPhoto.java
+++ Camping_Together/src/main/java/kr/or/iei/usedBoard/model/vo/UsedBoardPhoto.java
@@ -1,5 +1,15 @@
 package kr.or.iei.usedBoard.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsedBoardPhoto {
+	private int fileNo;
+	private int usedBoardNo;
+	private String filepath;
 
 }
