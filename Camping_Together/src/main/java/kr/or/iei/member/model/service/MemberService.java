78609cf9a10827eeed0b6ac803b843d314dde363
hamburger05 <gjtotquf53@gmail.com>
2023-04-06 12:00:05 +0900

    camping_paylist
    캠핑장 결제 내역 리스트

--- Camping_Together/src/main/java/kr/or/iei/member/model/service/MemberService.java
+++ Camping_Together/src/main/java/kr/or/iei/member/model/service/MemberService.java
@@ -1,11 +1,15 @@
 package kr.or.iei.member.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.iei.member.model.dao.MemberDao;
+import kr.or.iei.member.model.vo.CampingPayment;
 import kr.or.iei.member.model.vo.Member;
+import kr.or.iei.member.model.vo.MemberPageData;
 
 @Service
 public class MemberService {
@@ -28,6 +32,13 @@ public class MemberService {
 	//회원탈퇴
 	public int deleteMember(int memberNo) {
 		return dao.deleteMember(memberNo);
+	}
+	
+	public ArrayList<CampingPayment> selectPayList(int memberNo) {
+		ArrayList<CampingPayment> list = new ArrayList<CampingPayment>();
+		list = dao.selectPayList(memberNo);
+		return list;
+
 	}
 	
 	
