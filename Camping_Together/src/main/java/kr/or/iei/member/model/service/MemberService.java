7010898bc6b422ca2336640249024bbd948c615f
khsp1989 <126125441+khsp1989@users.noreply.github.com>
2023-04-19 15:05:52 +0900

    wishList 리스트

--- Camping_Together/src/main/java/kr/or/iei/member/model/service/MemberService.java
+++ Camping_Together/src/main/java/kr/or/iei/member/model/service/MemberService.java
@@ -304,7 +304,7 @@ public class MemberService {
 	}
 
 	public ShopListMainData selectWishList(int reqPage, String memberId) {
-		int numPerPage = 6;
+		int numPerPage = 8;
 		int end = reqPage * numPerPage;
 		int start = end - numPerPage + 1;
 		HashMap<String, Object> map = new HashMap<String, Object>();
@@ -322,13 +322,13 @@ public class MemberService {
 		}
 		String pageNavi = "<ul class='pagination circle-style'>";
 		if(pageNo != 1) {
-			pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+(pageNo-1)+"'><span class='material-symbols-outlined'>chevron_left</span></a></li>";
+			pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+(pageNo-1)+"&memberId="+memberId+"'><span class='material-symbols-outlined'>chevron_left</span></a></li>";
 		}
 			for(int i=0; i<pageNaviSize; i++) {
 				if(pageNo == reqPage) {
 					pageNavi += "<li><a class='page-item active-page'>"+pageNo+"</a></li>";
 				}else {
-					pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+pageNo+"'>"+pageNo+"</a></li>";
+					pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+pageNo+"&memberId="+memberId+"'>"+pageNo+"</a></li>";
 				}
 				pageNo++;
 				if(pageNo > totalPage) {
@@ -336,7 +336,7 @@ public class MemberService {
 				}
 			}
 		if(pageNo <= totalPage) {
-			pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+(pageNo+1)+"'><span class='material-symbols-outlined'>chevron_right </span></a></li>";
+			pageNavi += "<li><a class='page-item' href='/shopWishList.do?reqPage="+(pageNo+1)+"&memberId="+memberId+"'><span class='material-symbols-outlined'>chevron_right </span></a></li>";
 		}
 		ShopListMainData slmd = new ShopListMainData(list, pageNavi);
 		return slmd;
